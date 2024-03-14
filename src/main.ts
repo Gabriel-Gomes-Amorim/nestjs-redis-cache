7d2c4ad040c70d6499c17a0353e8b16ca21ec850
Gabriel-Gomes-Amorim <gomez.amorim18@hotmail.com>
2024-03-13 23:46:28 -0300

    adicionado config class validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurando o uso de pipes globais para validação de dados
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // Transforma os payloads recebidos em objetos das classes correspondentes
+      whitelist: true, // Remove propriedades não definidas nos DTOs
+      forbidNonWhitelisted: true, // Rejeita payloads com propriedades não definidas nos DTOs
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
